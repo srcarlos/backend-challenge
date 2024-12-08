4d6094dc9d46b5e6a132147a24972ef2053b23ac
srcarlos <clmg1010@gmail.com>
2024-12-08 16:39:38 -0300

    feat: added ordes, users, prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { OrdersModule } from './orders/orders.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, OrdersModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
